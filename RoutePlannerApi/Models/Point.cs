d944ab111f0680c83e13a31c3dddacb2f6d5a0ee
STEPiK0911 <136521792+STEPiK0911@users.noreply.github.com>
2024-07-21 01:48:21 +0300

    Update Point.cs

--- RoutePlannerApi/Models/Point.cs
+++ RoutePlannerApi/Models/Point.cs
@@ -3,7 +3,7 @@ namespace RoutePlannerApi.Models
     public class Point
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Height { get; set; }
     }
 }
